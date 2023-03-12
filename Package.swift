d607dbb8550a6ea20492c9fb15c5c0055ff557f8
evgeniyChepelev <e.chepelev@itransition.com>
2023-03-12 22:03:25 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,13 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "Dsds",
+    name: "Test",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Dsds",
-            targets: ["Dsds"]),
-        .library(name: "fdsf4", targets: ["fdsf4"])
+            name: "Library1",
+            targets: ["Library1"]),
+        .library(name: "Library12",
+                 targets: ["Library12"])
     ],
     dependencies: [
         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "10.4.0"))
@@ -21,17 +22,17 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Dsds",
+            name: "Library1",
             dependencies: [
                 .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseMessaging", package: "firebase-ios-sdk")
             ]),
         .target(
-            name: "fdsf4",
+            name: "Library12",
             dependencies: []
         ),
         .testTarget(
             name: "DsdsTests",
-            dependencies: ["Dsds"]),
+            dependencies: ["Library1"]),
     ]
 )
